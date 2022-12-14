8df60b37f3acddf27a53fc0fa53d3d9fac3749c5
Roman <romantimilsina@gmail.com>
2022-12-14 10:35:03 +0545

    added-PokemonList-Component

---
+++ src/PokemonList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pokemon from './Pokemon'
+
+export default function PokemonList({pokemons}) {
+  return (
+    <div>
+      {pokemons.map((pokemon) => {
+        return  <Pokemon key={pokemon} pokemon = {pokemon}/>
+      })}
+    </div>
+  )
+}
